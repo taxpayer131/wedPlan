65be660832e39faa5afa0d9596d9feaa18ddfe23
taxpayer131.Chris <taxpayer131.chris.stefan1844@gmail.com>
2017-11-28 15:33:05 -0800

    first commit

---
+++ wedPlan/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace wedPlan.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
+public class ApplicationRole : IdentityRole<int>
+{
+}
---
+++ wedPlan/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace wedPlan.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
